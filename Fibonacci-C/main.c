b16138546b15e3bbe074495e28acf5a5252130e5
Jinthebandit <jinthebandit@gmail.com>
2019-10-09 14:22:31 +0200

    added description

---
+++ Fibonacci-C/main.c
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.c
+ * Author: Lukas Fischer
+ * GitHub: https://github.com/Jinthebandit/Fibonacci-C.git
+ * 
+ * REQUIREMENTS HW-04:
+ * + berechne die ersten n Fibonacci Zahlen
+ * + benutze Datentyp int fuer die Berechnung
+ * 
+ * DEVELOPMENT CYCLE:
+ * Gesucht: Fibonacci Rechner
+ * Gegeben: Eingabe n-Wertes durch Benutzer
+ * Vorgehen:
+ *      1. Globale Variable erstellen: "FibType" um Datentyp einfach zu aendern
+ *      2. Funktion fuer Eingabe
+ *      3. Funktion fuer Ueberpruefung der Eingabe
+ *      5. Funktion fuer Ausgabe der Fibonacci-Folge
+ * 
+ * Created on 02. October, 11:21
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
